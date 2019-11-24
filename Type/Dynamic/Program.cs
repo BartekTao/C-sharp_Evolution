25a18d48b70a262bdcde143301ab3cc813d756d1
BartekTao <pony830906@gmail.com>
2019-11-24 22:07:43 +0800

    add dynamid example

--- Type/Dynamic/Program.cs
+++ Type/Dynamic/Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿#define example2
+using System;
 
 namespace Dynamic
 {
     class Program
     {
+#if example1
         static void Main(string[] args)
         {
             dynamic dyn = 1;
@@ -14,5 +16,29 @@ namespace Dynamic
             System.Console.WriteLine(dyn.GetType());
             System.Console.WriteLine(obj.GetType());
         }
+#endif
+
+#if example2
+        static void Main(string[] args)
+        {
+            dynamic dynInt = 1;
+            dynamic dynString = "thisIsString";
+            int intValue = 1;
+
+            addValue(intValue);
+            addValue(dynInt);
+            addValue(dynString);
+#if example3
+            string stringValue = "thisIsAnotherString";
+            addValue(stringValue);
+#endif
+
+            static void addValue(int value)
+            {
+                Console.WriteLine(value + value);
+            }
+        }
+#endif
+
     }
 }
