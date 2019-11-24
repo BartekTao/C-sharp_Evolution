cad98ba14041217de888d97358c7b3f1e43612fe
BartekTao <pony830906@gmail.com>
2019-11-24 18:29:08 +0800

    init lesson 1

---
+++ ProgramStructure/ProgramStructure1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProgramStructure1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+namespace ProgramStructure1.Data
+{
+    class Customer
+    {
+        string Name { get; set; }
+
+        string Id { get; set; }
+    }
+}
